3e8a6847d8745d8fb63fc5e103bdf271b485685f
Astar <bahouidi99@gmail.com>
2023-12-18 16:59:53 +0100

    Fix CORS issues

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const workoutsRouter = require("./routes/workouts.routes")
 
@@ -11,6 +12,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log({ path: req.path, method: req.method });
   next();
